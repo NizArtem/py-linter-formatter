bea0078d92d04bd6372e7b6c2ef59d7a7416cd95
Artem Nizamiiev <upuyayua@gmail.com>
2025-09-22 00:41:55 +0300

    solved the task and added docstrings

--- app/main.py
+++ app/main.py
@@ -1,4 +1,10 @@
 def format_linter_error(error: dict) -> dict:
+    """
+       Formatting single error to correct format
+
+       :param error: dict with error
+       :return: formatted dict with correct keys
+       """
     return {"line": error["line_number"],
             "column": error["column_number"],
             "message": error["text"],
@@ -7,6 +13,15 @@ def format_linter_error(error: dict) -> dict:
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
+    """
+    Formatting list with errors and file
+    path where it was to a dict
+
+    :param
+        file_path: string with file path where was error
+        errors: list with unformatted errors
+    :return: formatted dict with correct keys and path
+    """
     return {"errors":
             [format_linter_error(error) for error in errors],
             "path": file_path,
@@ -15,5 +30,11 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 
 def format_linter_report(linter_report: dict) -> list:
+    """
+        Formattind dictionary report to correct format
+
+        :param linter_report: unformatted linter report
+        :return: formatted list with dictionaries of reports
+        """
     return [format_single_linter_file(path, errors)
             for path, errors in linter_report.items()]
